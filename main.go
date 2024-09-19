8c5ac37e8deadbb905236804d6e46ec3ffdb3780
script kiddie <anish.chittu2007@gmail.com>
2024-09-19 23:49:04 +0200

    <3

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"amgis/lexer"
 )
 
-func main(){
-	file ,err := os.ReadFile("D:\\sigma\\in\\test2.go")
-	if err!= nil {
-        fmt.Printf("Error reading file: %v\n", err)
-        return
-    }
-	
-    test := string(file)
-	os.WriteFile("/sigma/out/test2.sigma", []byte(lexer.Words(test)), 0644)
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: gosigma build <input.go> <output.sigma>")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	file, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	test := string(file)
+	err = os.WriteFile(outputFile, []byte(lexer.Words(test)), 0644)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Successfully processed %s and wrote to %s\n", inputFile, outputFile)
 }
